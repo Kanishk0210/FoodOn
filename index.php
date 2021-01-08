108d545d7e0b872da909a29b600b3c47d3d13063
kanishk0210 <kanishk0210>
2021-01-08 19:55:24 +0530

    in progress

---
+++ index.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include("connection/connect.php");  //include connection file
+error_reporting(0);  // using to hide undefine undex errors
+session_start(); //start temp session until logout/browser closed
+
+?>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="#">
+    <title>FoodOn</title>
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/animsition.min.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet"> </head>
+
+<body class="home">
+    <div class="site-wrapper animsition" data-animsition-in="fade-in" data-animsition-out="fade-out">
+        <!--header starts-->
+        <header id="header" class="header-scroll top-header headrom">
+            <!-- .navbar -->
+            <nav class="navbar navbar-dark ">
+                <div class="container">
+                    <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#mainNavbarCollapse">&#9776;</button>
+                    <a class="navbar-brand" href="index.php"> <h> FoodOn</h> </a>
+                    <div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
+                        <ul class="nav navbar-nav">
+                            <li class="nav-item"> <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
+                            <li class="nav-item"> <a class="nav-link active" href="restaurants.php">Restaurants <span class="sr-only"></span></a> </li>
+
+                            <li class="nav-item"> <a class="nav-link active" href="contactus.php">Contact Us <span class="sr-only">(current)</span></a> </li>
+
+                            <li class="nav-item"> <a class="nav-link active" href="aboutus.php">About Us <span class="sr-only">(current)</span></a> </li>
+                            
+                           
+							<?php
+						if(empty($_SESSION["user_id"])) // if user is not login
+							{
+								echo '<li class="nav-item"><a href="login.php" class="nav-link active">Login</a> </li>
+							  <li class="nav-item"><a href="registration.php" class="nav-link active">Signup</a> </li>';
+							}
+						else
+							{
+									//if user is login
+									
+									echo  '<li class="nav-item"><a href="your_orders.php" class="nav-link active">your orders</a> </li>';
+									echo  '<li class="nav-item"><a href="logout.php" class="nav-link active">logout</a> </li>';
+							}
+
+						?>
+							 
+                        </ul>
+						 
+                    </div>
+                </div>
+            </nav>
+            <!-- /.navbar -->
+        </header>
+        <!-- banner part starts -->
+        <section class="hero bg-image" data-image-src="images/img/image12.jpg">
+            <div class="hero-inner">
+                <div class="container text-center hero-text font-white">
+                    <h1>Order & Get Food Online </h1>
+                    <h5 class="font-white space-xs"></h5>
+                    <div class="banner-form">
+                        
+                            <button onclick="location.href='restaurants.php'" type="button" class="btn theme-btn btn-lg">Order Now</button>
+   
+                    </div>
+
+                    <!-- end:Steps -->
+                </div>
+            </div>
+            <!--end:Hero inner -->
+        </section>
+        <!-- banner part ends -->
+    <!--  
+	                              <button onclick="location.href='special.php'" type="button" class="btn theme-btn btn-lg">Todays Special</button>
+	                                    
+	                              <button onclick="location.href='special.php'" type="button" class="btn theme-btn btn-lg">Todays Special</button>
+	                                    
+	                              <button onclick="location.href='special.php'" type="button" class="btn theme-btn btn-lg">Todays Special</button>
+	                                    
+	                              <button onclick="location.href='special.php'" type="button" class="btn theme-btn btn-lg">Todays Special</button>
+	                                    
+	                              <button onclick="location.href='special.php'" type="button" class="btn theme-btn btn-lg">Todays Special</button>
+	                                    
+	                              <button onclick="location.href='special.php'" type="button" class="btn theme-btn btn-lg">Todays Special</button>
+	                                    
+	                              <button onclick="location.href='special.php'" type="button" class="btn theme-btn btn-lg">Todays Special</button>
+	   -->
+
+        <!-- start: FOOTER -->
+        <footer class="footer">
+            <div class="container">
+                <!-- top footer statrs -->
+
+                <!-- bottom footer statrs -->
+                <div class="bottom-footer">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-3 payment-options color-gray">
+                            <h5>Payment Options</h5>
+                            <ul>
+                                <li>
+                                    <a href="#"> <img src="images/paypal.png" alt="Paypal"> </a>
+                                </li>
+                                <li>
+                                    <a href="#"> <img src="images/mastercard.png" alt="Mastercard"> </a>
+                                </li>
+                                <li>
+                                    <a href="#"> <img src="images/maestro.png" alt="Maestro"> </a>
+                                </li>
+                                <li>
+                                    <a href="#"> <img src="images/stripe.png" alt="Stripe"> </a>
+                                </li>
+                                <li>
+                                    <a href="#"> <img src="images/bitcoin.png" alt="Bitcoin"> </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-xs-12 col-sm-4 address color-gray">
+                            <h5>Address</h5>
+                            <p>Concept design of oline food order and deliveye,planned as restaurant directory</p>
+                            <h5>Phone: <a href="tel:+917974554958">+917974554958</a></h5> </div>
+                        <div class="col-xs-12 col-sm-5 additional-info color-gray">
+                            <h5>Addition informations</h5>
+                            <p>Join the thousands of other restaurants who benefit from having their menus on TakeOff</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- bottom footer ends -->
+            </div>
+        </footer>
+        <!-- end:Footer -->
+    </div>
+    <!--/end:Site wrapper -->
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/tether.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/animsition.min.js"></script>
+    <script src="js/bootstrap-slider.min.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/headroom.js"></script>
+    <script src="js/foodpicky.min.js"></script>
+</body>
+
+</html>
